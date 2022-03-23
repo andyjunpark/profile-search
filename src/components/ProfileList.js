ce1fe690d3f30e72e16ee70015f56e944d08f8e5
Andy Junhyuk Park <pos03142ap@gmail.com>
2022-03-23 15:01:09 -0700

    add a collapsible container with toggle button

--- src/components/ProfileList.js
+++ src/components/ProfileList.js
@@ -1,8 +1,18 @@
 import React, {useRef} from "react";
+import { useState } from "react";
+import { FiPlus, FiMinus } from 'react-icons/fi'
 import "./ProfileList.css";
 
 export default function ProfileList(props) {
+  const [clicked, setClicked] = useState(false);
   const inputEl = useRef("");
+
+  const toggle = index => {
+    if(clicked === index) {
+      return setClicked(null);
+    }
+    setClicked(index);
+  }
 
   // returns the average score of grades
   const getAverageGrades = (grades) => {
@@ -25,30 +35,44 @@ export default function ProfileList(props) {
   const term = props.searchTerm;
 
   // populate profiles
-  const profileList = profile.map(profiles => {
+  const profileList = profile.map((profiles, index) => {
     return (
       <div className="profile">
         <img 
           src={profiles.pic} 
           alt="profile" 
         />
-        <div key={profiles.id}>
+        <div className="profile-info" key={profiles.id}>
           <h1>{profiles.firstName.toUpperCase()} {profiles.lastName.toUpperCase()}</h1>
           <ul>
-            <li>{profiles.email}</li>
-            <li>{profiles.company}</li>
-            <li>{profiles.skill}</li>
-            <li>{getAverageGrades(profiles.grades)}%</li>
+            <li>Email: {profiles.email}</li>
+            <li>Company: {profiles.company}</li>
+            <li>Skill: {profiles.skill}</li>
+            <li>Average {getAverageGrades(profiles.grades)}%</li>
           </ul>
           <ul>
-            {
-              profiles.grades.map((grade, index) => {
-                return (
-                  <li>Test {index+1}: &nbsp; &nbsp; {grade}%</li>
-                )
-              })
+            { 
+              clicked === index 
+                ? ( profiles.grades.map((grade, index) => {
+                    return (
+                      <li>Test {index+1}: &nbsp; &nbsp; {grade}%</li>
+                    )
+                  })) 
+                : null
             }
           </ul>
+        </div>
+        <div className="icons" onClick={() => toggle(index)} key={index}>
+          <span>
+            {
+              clicked === index 
+                ? <FiMinus size={40} 
+                onMouseOver={({target})=>target.style.color="black"}
+                onMouseOut={({target})=>target.style.color="grey"}/> 
+                : <FiPlus size={40}
+                />
+            }
+          </span>
         </div>
       </div>
     )
@@ -61,7 +85,6 @@ export default function ProfileList(props) {
           ref={inputEl}
           type="text" 
           placeholder="Search by name" 
-          className="prompt" 
           value={term} 
           onChange={getSearchTerm}
         >
